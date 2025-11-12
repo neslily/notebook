bfa95b0ca4cdc8634da072633c29ab0416cf5e45
neslily <unlneslihan74@gmail.com>
2025-11-12 23:25:38 +0300

    notebook

---
+++ not_defteri.py
@@ -0,0 +1,37 @@
+def not_defteri():
+    while True:
+        print("   NOT DEFTERİ")
+        print("1:notları listele")
+        print("2:not ekle")
+        print("3:notları sil")
+        print("4.çıkış")
+
+        secim=int(input("seçiminizi yapın:"))
+
+        if secim==1:
+            print("Notlar:")
+            try:
+                with open("notlar.txt","r") as file:
+                    print(file.read())
+            except FileNotFoundError:
+                print("henüz not eklenmemiş")
+
+        elif secim==2:
+            not_metni=input("notunuzu girin:")
+            with open("notlar.txt","a") as file:
+                file.write(not_metni+"\n")
+            print("not başarıyla kaydedildi")
+
+        elif secim==3:
+            print("notlarınız siliniyor")
+            try:
+                with open("notlar.txt","w") as file:    # Dosyayı yazma modunda açıyoruz, içerik silinir
+                    pass  # İçeriği boş bırakıyoruz, dosya temizlenir
+                print("notlar silindi")
+            except FileNotFoundError:
+                print("henüz not eklenmemiş")
+
+        else:
+            print("sistemden çıkılıyor")
+            break
+not_defteri()
